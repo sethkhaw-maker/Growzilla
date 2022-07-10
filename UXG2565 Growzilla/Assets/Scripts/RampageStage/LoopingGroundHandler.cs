bcf8c74e88ce5f00e9f6350cf6dee1b245b4d06d
lokeki1698 <lokeki1698@gmail.com>
2022-07-10 12:32:18 +0800

    Added looping background and ground.

---
+++ UXG2565 Growzilla/Assets/Scripts/RampageStage/LoopingGroundHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LoopingGroundHandler : MonoBehaviour
+{
+    BoxCollider2D GroundCollider = null;
+    private float groundLength = 0.0f;
+
+    void Awake()
+    {
+        GroundCollider = GetComponent<BoxCollider2D>();
+        groundLength = GroundCollider.size.x;
+    }
+
+    void Update()
+    {
+        if (transform.position.x < -groundLength)
+        {
+            Loop();
+        }
+    }
+    void Loop()
+    {
+        Vector2 groundOffset = new Vector2(groundLength * 2f, 0.0f);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
---
+++ UXG2565 Growzilla/Assets/Scripts/RampageStage/ScrollingObjectsHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ScrollingObjectsHandler : MonoBehaviour
+{
+    [SerializeField] private float ScrollSpeed = 1.0f;
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(-ScrollSpeed, 0);
+    }
+}
