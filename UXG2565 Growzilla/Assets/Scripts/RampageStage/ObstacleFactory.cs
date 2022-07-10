52da8ee26cb45c88dde59ad9874f764ffd524045
lokeki1698 <lokeki1698@gmail.com>
2022-07-10 17:00:44 +0800

    Added first obstacle, the generic building that can be destroyed by any ability.

---
+++ UXG2565 Growzilla/Assets/Scripts/RampageStage/ObstacleFactory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ObstacleFactory : MonoBehaviour
+{
+    [SerializeField] Transform Obstacle1SpawnPosition = null;
+    [SerializeField] Vector2 Obstacle1Cooldown = new Vector2(1.0f, 2.0f);
+    [SerializeField] GameObject Obstacle1Prefab = null;
+    float obstacle1CooldownTimer = 0.0f;
+
+    void Start()
+    {
+        obstacle1CooldownTimer = Random.Range(Obstacle1Cooldown.x, Obstacle1Cooldown.y);
+    }
+    void Update()
+    {
+        Obstacle1Tick();    
+    }
+    void Obstacle1Tick()
+    {
+        if (Obstacle1Prefab == null) { return; }
+        if (obstacle1CooldownTimer > 0.0f) { obstacle1CooldownTimer -= Time.deltaTime; return; }
+
+        Instantiate(Obstacle1Prefab, Obstacle1SpawnPosition.position, Quaternion.identity);
+
+        obstacle1CooldownTimer = Random.Range(Obstacle1Cooldown.x, Obstacle1Cooldown.y);
+    }
+}
---
+++ UXG2565 Growzilla/Assets/Scripts/RampageStage/Obstacles/ObstacleGenericBuilding.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ObstacleGenericBuilding : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2.0f;
+    Rigidbody2D rb2d = null;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();    
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = moveSpeed * -Vector2.right;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "BigKaijuu":
+            case "MidKaijuu":
+            case "SmallKaijuu":
+                // Deal Damage to Kaijuu Here.
+                // Add Destruction Score
+                Debug.Log("Kaijuu Receives Damage!");
+                Destroy(this.gameObject);
+                break;
+            case "ability_horns_hitbox":
+            case "ability_tail_hitbox":
+            case "ability_wings_hitbox":
+                // Add Destruction score.
+                Debug.Log("Kaijuu Used Skill to Destroy Buildings!");
+                Destroy(this.gameObject);
+                break;
+            case "SceneBorders":
+                Destroy(this.gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+}
