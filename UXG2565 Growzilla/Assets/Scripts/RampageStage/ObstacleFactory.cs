cb9fb7ac7355565545f5a54bffec565c7a3adc73
lokeki1698 <lokeki1698@gmail.com>
2022-07-14 17:47:25 +0800

    Landmines functionality

--- UXG2565 Growzilla/Assets/Scripts/RampageStage/ObstacleFactory.cs
+++ UXG2565 Growzilla/Assets/Scripts/RampageStage/ObstacleFactory.cs
@@ -2,7 +2,7 @@
 
 public class ObstacleFactory : MonoBehaviour
 {
-    [SerializeField] float MinimumCooldown = 1.0f;
+    [SerializeField] Vector2 MinimumCooldown = new Vector2(1.0f, 2.0f);
     float obstacleMinimumCooldown = 0.0f;
 
     [SerializeField] Transform Obstacle1SpawnPosition = null;
@@ -15,16 +15,23 @@ public class ObstacleFactory : MonoBehaviour
     [SerializeField] GameObject Obstacle2Prefab = null;
     float obstacle2CooldownTimer = 0.0f;
 
+    [SerializeField] Transform Obstacle3SpawnPosition = null;
+    [SerializeField] Vector2 Obstacle3Cooldown = new Vector2(1.0f, 2.0f);
+    [SerializeField] GameObject Obstacle3Prefab = null;
+    float obstacle3CooldownTimer = 0.0f;
+
     void Start()
     {
         obstacle1CooldownTimer = Random.Range(Obstacle1Cooldown.x, Obstacle1Cooldown.y);
         obstacle2CooldownTimer = Random.Range(Obstacle2Cooldown.x, Obstacle2Cooldown.y);
+        obstacle3CooldownTimer = Random.Range(Obstacle3Cooldown.x, Obstacle3Cooldown.y);
     }
     void Update()
     {
         obstacleMinimumCooldown -= Time.deltaTime;
         Obstacle1Tick();
         Obstacle2Tick();
+        Obstacle3Tick();
     }
     void Obstacle1Tick()
     {
@@ -37,7 +44,7 @@ public class ObstacleFactory : MonoBehaviour
 
         obstacle1CooldownTimer = Random.Range(Obstacle1Cooldown.x, Obstacle1Cooldown.y);
 
-        obstacleMinimumCooldown = MinimumCooldown;
+        obstacleMinimumCooldown = Random.Range(MinimumCooldown.x, MinimumCooldown.y);
     }
     void Obstacle2Tick()
     {
@@ -50,6 +57,20 @@ public class ObstacleFactory : MonoBehaviour
 
         obstacle2CooldownTimer = Random.Range(Obstacle2Cooldown.x, Obstacle2Cooldown.y);
 
-        obstacleMinimumCooldown = MinimumCooldown;
+        obstacleMinimumCooldown = Random.Range(MinimumCooldown.x, MinimumCooldown.y);
+    }
+
+    void Obstacle3Tick()
+    {
+        if (obstacleMinimumCooldown > 0.0f) { return; }
+
+        if (Obstacle3Prefab == null) { return; }
+        if (obstacle3CooldownTimer > 0.0f) { obstacle3CooldownTimer -= Time.deltaTime; return; }
+
+        Instantiate(Obstacle3Prefab, Obstacle3SpawnPosition.position, Quaternion.identity);
+
+        obstacle3CooldownTimer = Random.Range(Obstacle3Cooldown.x, Obstacle3Cooldown.y);
+
+        obstacleMinimumCooldown = Random.Range(MinimumCooldown.x, MinimumCooldown.y);
     }
 }
---
+++ UXG2565 Growzilla/Assets/Scripts/RampageStage/Obstacles/ObstacleLandmine.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleLandmine : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2.0f;
+    Rigidbody2D rb2d = null;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        GetComponent<Animator>().Play("LandmineIdle");
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = moveSpeed * -Vector2.right;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "BigKaijuu":
+            case "MidKaijuu":
+            case "SmallKaijuu":
+                // Deal Damage to Kaijuu Here.
+                // Add Destruction Score
+                Debug.Log("Kaijuu Receives Damage from Landmines!");
+                GetComponent<Animator>().Play("LandmineTrigger");
+                GetComponent<Collider2D>().enabled = false;
+                Invoke("OnDestroyed", 1.0f);
+                break;
+            case "ability_tail_hitbox":
+                // Add Destruction score.
+                Debug.Log("Kaijuu Used Skill to Destroy Landmines!");
+                GetComponent<Animator>().Play("LandmineTrigger");
+                GetComponent<Collider2D>().enabled = false;
+                Invoke("OnDestroyed", 1.0f);
+                break;
+            case "SceneBorders":
+                Destroy(this.gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+    void OnDestroyed()
+    {
+        Destroy(this.gameObject);
+    }
+}
